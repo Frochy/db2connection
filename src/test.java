a5a769abd30a31e45c1618fba240e608eccba03d
Frochy <ztyzqq@outlook.com>
2018-12-15 22:56:04 +0800

    IDEA github test

--- src/test.java
+++ src/test.java
@@ -1,17 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        /**
-         * : <br>
-         * <></>
-         *
-         * @param args
-         * @return:void
-         * @since: 1.0.0
-         * @Author:zhangyong
-         * @Date: 2018/12/15 20:51
-         */
 
-        System.out.println("helloTestaaa");
 
     }
 }
